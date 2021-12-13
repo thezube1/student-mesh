24a51848dd527ec1bec67aec8c78c90013f31702
thezube <zubin.hydrie@bush.edu>
2021-12-13 11:03:18 -0800

    Added event tracking

--- client/components/contracts/GetStudent.jsx
+++ client/components/contracts/GetStudent.jsx
@@ -4,25 +4,44 @@ class GetStudents extends React.Component {
   state = {
     dataKey: null,
     data: undefined,
+    events: undefined,
   };
 
   componentDidMount() {
     const { drizzle, drizzleState } = this.props;
-    console.log(drizzle);
     const contract = drizzle.contracts.Students;
     const dataKey = contract.methods["get"].cacheCall();
     this.setState({ dataKey });
+    this.getPastEvents();
   }
+
+  componentDidUpdate() {
+    this.getPastEvents();
+  }
+
+  getPastEvents = async () => {
+    const web3 = this.props.drizzle.web3;
+    const contract = this.props.drizzle.contracts.Students;
+    const yourContractWeb3 = new web3.eth.Contract(
+      contract.abi,
+      contract.address
+    );
+    const data = await yourContractWeb3.getPastEvents("Student", {
+      fromBlock: 0,
+      toBlock: "latest",
+    });
+    this.setState({ events: data });
+  };
   render() {
-    const { Students } = this.props.drizzleState.contracts;
-    const student_array = Students.get[this.state.dataKey];
-    if (student_array === undefined) {
+    // const { Students } = this.props.drizzleState.contracts;
+    // const student_array = Students.get[this.state.dataKey];
+    if (this.state.events === undefined) {
       return <div>Loading...</div>;
     } else {
       return (
         <div>
-          {student_array.value.map((item, index) => {
-            return <div key={index}>{item}</div>;
+          {this.state.events.map((item, index) => {
+            return <div key={index}>{item.returnValues._value}</div>;
           })}
         </div>
       );
