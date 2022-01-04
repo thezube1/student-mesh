db5f287fa12c6f28bc0f5d27c4d0bf03beb044a6
Zubin <zubin.hydrie@bush.edu>
2022-01-04 13:17:39 -0800

    Push latest

--- contracts/Students.sol
+++ contracts/Students.sol
@@ -7,10 +7,14 @@ contract Students {
     // owner is the wallet which the file will be linked to
     // address is the physical address where the file lives on the IPFS
     event RequestApproval(address indexed provider, address indexed reciever, string header);
-    event ApproveRequest(address indexed owner, address indexed provider, string header);
+    event ApproveRequest(address indexed owner, address indexed provider, string header, string location);
     
-    function createRequest(address _reciever, string memory _header) public {
+    function request(address _reciever, string memory _header) public {
         emit RequestApproval(msg.sender, _reciever, _header);
+    }
+
+    function approve(address _provider, string memory _header, string memory _location) public {
+        emit ApproveRequest(msg.sender, _provider, _header, _location);
     }
 }
 
