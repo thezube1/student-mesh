29fe7898c38388a120386a7c15fb2f61ee765766
Zubin <zubin.hydrie@bush.edu>
2022-02-28 18:58:10 -0800

    Added second home page

--- client/components/home/default/DefaultHome.js
+++ client/components/home/default/DefaultHome.js
@@ -1,7 +1,10 @@
 import Navbar from "../../navbar/navbar";
-import WaveSVG from "../default/WaveSVG";
+import WaveSVG from "./WaveSVG";
+import Wave2SVG from "./Wave2SVG";
+import DistributedSVG from "./DistributedSVG";
 import Link from "next/link";
 import { useEffect } from "react";
+import * as Scroll from "react-scroll";
 
 function DefaultHome() {
   useEffect(() => {
@@ -44,26 +47,64 @@ function DefaultHome() {
             <Link href="/connect">
               <button className="button home-button">Connect Account</button>
             </Link>
-
-            <button className="button home-button-secondary">
-              How It Works
-            </button>
+            <Scroll.Link to="home-wrapper-2" smooth={true}>
+              <button className="button home-button-secondary">
+                How It Works
+              </button>
+            </Scroll.Link>
           </div>
         </div>
       </div>
       <div id="home-wrapper-2">
-        <div className="title home2-header">
-          Transcripts are recorded to the Ethereum blockchain, meaning:
+        <div
+          style={{
+            display: "flex",
+            gap: 100,
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          <div style={{ display: "grid", justifyItems: "center" }}>
+            <div className="title home2-header" style={{ fontSize: 35 }}>
+              Transcripts are recorded to the Ethereum blockchain, meaning:
+            </div>
+            <div className="title home2-header">
+              Transcripts are <span className="primary-text">immutable</span>{" "}
+            </div>
+            <div className="title home2-header">
+              They are publicly <span className="primary-text">verifiable</span>{" "}
+            </div>
+            <div className="title home2-header">
+              Each transcript is{" "}
+              <span className="primary-text">user-controlled</span>{" "}
+            </div>{" "}
+          </div>
+          <div style={{ maxWidth: 400, width: "90%" }}>
+            <DistributedSVG />
+          </div>
         </div>
-        <div className="title home2-header">
-          Transcripts are <span className="primary-text">immutable</span>{" "}
-        </div>
-        <div className="title home2-header">
-          They are publicly <span className="primary-text">verifiable</span>{" "}
-        </div>
-        <div className="title home2-header">
-          Each transcript is{" "}
-          <span className="primary-text">user-controlled</span>{" "}
+        {/* <div>
+          <div className="title home2-sub-header">
+            Students choose who gets to see their transcripts, and what happens
+            to them
+          </div>
+          <div className="title home2-sub-header">
+            Schools are no longer required to store transcripts and retreive
+            them on request
+          </div>
+        </div>*/}
+
+        <div
+          style={{
+            position: "absolute",
+            zIndex: -1,
+            transform: "translateY(-50%)",
+
+            top: "80%",
+            width: "100%",
+          }}
+        >
+          <Wave2SVG />
         </div>
       </div>
     </div>
