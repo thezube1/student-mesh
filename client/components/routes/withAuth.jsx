160339fd3bc5825183925ad1c24dfd0cc5a5a48a
Zubin <zubin.hydrie@bush.edu>
2022-03-18 13:18:25 -0700

    Quick fix 2

--- client/components/routes/withAuth.jsx
+++ client/components/routes/withAuth.jsx
@@ -1,14 +1,13 @@
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
+import React from "react";
 
-function withAuth(WrappedComponent) {
-  return (props) => {
+const withAuth = (WrappedComponent) => {
+  const AuthRoute = (props) => {
+    const Router = useRouter();
+    const address = useSelector((state) => state.account.account);
+    //const provider = useSelector((state) => state.account.provider);
     if (typeof window !== "undefined") {
-      const Router = useRouter();
-
-      const address = useSelector((state) => state.account.account);
-      const provider = useSelector((state) => state.account.provider);
-
       if (!web3 || !address) {
         Router.replace("/");
         return null;
@@ -22,6 +21,7 @@ function withAuth(WrappedComponent) {
     // If we are on server, return null
     return null;
   };
-}
+  return AuthRoute;
+};
 
 export default withAuth;
--- client/components/routes/withProvider.jsx
+++ client/components/routes/withProvider.jsx
@@ -1,26 +1,25 @@
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
+import React from "react";
 
 function withProvider(WrappedComponent) {
-  return (props) => {
+  const AuthRoute = (props) => {
+    const Router = useRouter();
+    const provider = useSelector((state) => state.account.provider);
+
     if (typeof window !== "undefined") {
-      const Router = useRouter();
-
-      const provider = useSelector((state) => state.account.provider);
-
       if (!provider.isProvider) {
         Router.replace("/");
         return null;
       }
-
       // If this is an accessToken we just render the component that was passed with all its props
-
       return <WrappedComponent {...props} />;
     }
 
     // If we are on server, return null
     return null;
   };
+  return AuthRoute;
 }
 
 export default withProvider;
